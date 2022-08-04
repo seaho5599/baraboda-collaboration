8d7bfef7d316fc6e23b4ee0772f9418f485824c8
Parktaehoon1 <92parkth@gmail.com>
2022-08-04 22:47:06 +0900

    코드 진행중

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     // 헤더 픽스하기
     let headerBottom = $('.header-bottom')
+    let headerTop = $('.header-top')
     let logoFix = $('.logo-fix')
     let headerBottomFix = $('.header-bottom-fix')
 
@@ -8,23 +9,25 @@ $(document).ready(function () {
     $(window).scroll(function () {
         let temp = $(window).scrollTop();
         console.log(temp);
-        if (temp > 100) {
+        if (temp > 50) {
+            headerTop.hide()
             headerBottom.addClass('header-bottom-fix')
             logoFix.css({
                 'width': '196px',
                 'opacity': '1',
                 'visibility': 'visible',
                 'margin-right': '56px',
-                'margin-top' : '15px'
+                'margin-top': '15px'
             });
         } else {
+            headerTop.show()
             headerBottom.removeClass('header-bottom-fix')
             logoFix.css({
                 'width': '0',
                 'opacity': '0',
                 'visibility': 'hidden',
                 'margin-right': '0',
-                'margin-top' : '0'
+                'margin-top': '0'
             });
             headerBottomFix.hide()
         }
@@ -44,4 +47,39 @@ window.onload = function () {
     //         height  : null,     /*  선택. max-height를 지정한다. 만약 null이면 알아서 잰다. */
     //         tolerance: 0       /*  글이 넘치면 이만큼쯤 height를 늘린다 */
     //       });
+
+    // 팝업 무한 반복 전광판 같음
+    // setInterval(function(){
+    //     $('.pop-up ul').delay(1000);
+    //     $('.pop-up ul').animate({marginTop: "0px"})
+    //     $('.pop-up ul').delay(1000);
+    //     $('.pop-up ul').animate({marginTop: "-40px"})
+    // });
+    
+    $('.pop-up>ul>li').hide();
+    $('.pop-up>ul>li:first-child').show();
+    
+    setInterval(function(){
+        $('.pop-up>ul>li:first-child').hide()
+        .next().show().end(1000)
+        .appendTo('.pop-up>ul');
+    },5000);
+
+
+    // 스와이퍼
+    new Swiper('.sw-visual',{
+        loop: true,
+        effect : 'fade',
+        speed: 500,
+        autoplay: {
+            delay: 3000,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        navigation: {
+            nextEl: ".sw-next-bt",
+            prevEl: ".sw-prev-bt",
+        },
+    })
 }
