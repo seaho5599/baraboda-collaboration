e843030c4e8fc6e102aa68b636da25005bada049
Parktaehoon1 <92parkth@gmail.com>
2022-08-09 13:49:49 +0900

    PC버전 완료

--- js/script.js
+++ js/script.js
@@ -31,97 +31,124 @@ $(document).ready(function () {
             });
             headerBottomFix.hide()
         }
-    })
-})
+    });
+});
 
 window.onload = function () {
-    AOS.init();
+  AOS.init();
+  //     $("#wrapper").dotdotdot({
+  //         wrapper  : 'div',  /*  콘텐트를 감쌀 요소. */
+  //         ellipsis: '... ',  /*  말줄임표를 뭘로 할지 */
+  //         wrap  : 'word',    /*  자를 단위. 다음 옵션 중 하나 선택: 'word'/'letter'/'children' */
+  //         after  : null,     /*  자르고 나서도 유지시킬 요소를 jQuery 선택자로 적는다. */
+  //         watch  : false,    /*  윈도우가 리사이즈될 때 업데이트할 건지: true/'window' */
+  //         height  : null,     /*  선택. max-height를 지정한다. 만약 null이면 알아서 잰다. */
+  //         tolerance: 0       /*  글이 넘치면 이만큼쯤 height를 늘린다 */
+  //       });
+
+  // 팝업 무한 반복 전광판 같음
+  // setInterval(function(){
+  //     $('.pop-up ul').delay(1000);
+  //     $('.pop-up ul').animate({marginTop: "0px"})
+  //     $('.pop-up ul').delay(1000);
+  //     $('.pop-up ul').animate({marginTop: "-40px"})
+  // });
+  
+  $('.pop-up>ul>li').hide();
+  $('.pop-up>ul>li:first-child').show();
+  
+  setInterval(function(){
+      $('.pop-up>ul>li:first-child').hide()
+      .next().show().end(1000)
+      .appendTo('.pop-up>ul');
+  },5000);
 
 
-
-    //     $("#wrapper").dotdotdot({
-    //         wrapper  : 'div',  /*  콘텐트를 감쌀 요소. */
-    //         ellipsis: '... ',  /*  말줄임표를 뭘로 할지 */
-    //         wrap  : 'word',    /*  자를 단위. 다음 옵션 중 하나 선택: 'word'/'letter'/'children' */
-    //         after  : null,     /*  자르고 나서도 유지시킬 요소를 jQuery 선택자로 적는다. */
-    //         watch  : false,    /*  윈도우가 리사이즈될 때 업데이트할 건지: true/'window' */
-    //         height  : null,     /*  선택. max-height를 지정한다. 만약 null이면 알아서 잰다. */
-    //         tolerance: 0       /*  글이 넘치면 이만큼쯤 height를 늘린다 */
-    //       });
-
-    // 팝업 무한 반복 전광판 같음
-    // setInterval(function(){
-    //     $('.pop-up ul').delay(1000);
-    //     $('.pop-up ul').animate({marginTop: "0px"})
-    //     $('.pop-up ul').delay(1000);
-    //     $('.pop-up ul').animate({marginTop: "-40px"})
-    // });
-
-
-    // 유지보수가 어려움
-    // $('.pop-up>ul>li').hide();
-    // $('.pop-up>ul>li:first-child').show();
-
-    // setInterval(function(){
-    //     $('.pop-up>ul>li:first-child').hide()
-    //     .next().show().end(1000)
-    //     .appendTo('.pop-up>ul');
-    // },5000);
-
-    // 유지보수 용이
-    let popLi = $('.pop-up>ul>li');
-    let popTotal = popLi.length;
-    let popShowIndex = 0;
-    let popShowTime = 4000;
-
-    function showPop() {
-        popShowIndex++; // index를 증가 시켜야지 li나옴
-        if (popShowIndex >= popTotal) {
-            popShowIndex = 0;
-        }
-        $.each(popLi, function (index, item) {
-            popLi.show()
-            popLi.eq(popShowIndex).hide()
-        })
+  // 비주얼 슬라이드
+  new Swiper('.sw-visual', {
+    effect: 'fade',
+    loop: true,
+    speed: 1000,
+    autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    },
+  
+    navigation: {
+      prevEl: '.sw-visual-prev',
+      nextEl: '.sw-visual-next'
+    },
+  
+    pagination: {
+      el: '.sw-visual-pg',
+      clickable: true
     }
+  });
 
-    setInterval(showPop, popShowTime);
-
-    // 비주얼 슬라이드
-    new Swiper('.sw-visual', {
-        effect: 'fade',
-        loop: true,
-        speed: 1000,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-
-        navigation: {
-            prevEl: '.sw-visual-prev',
-            nextEl: '.sw-visual-next'
-        },
-
-        pagination: {
-            el: '.sw-visual-pg',
-            clickable: true
-        }
+  // 공지사항 슬라이드
+  let sw_notice = new Swiper('.sw-notice', {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 30,
+      navigation: {
+        prevEl: '.sw-notice-prev',
+        nextEl: '.sw-notice-next'
+      },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
     });
+  
+  // 아이템 리스트 배열
+  let itemGoodsArr = [{
+    link: '#',
+    pic: 'item-goods-0.png',
+    title: '바라보다 콜롬비아 아구아 프레스카 핑크버번 내추럴',
+    info: '바나나, 히비스커스, 쥬시, 슈가케인',
+    price: '21,000원',
+    color: '#ecba19'
+  }, {
+    link: '#',
+    pic: 'item-goods-1.png',
+    title: '바라보다 에티오피아 구지 G1 담비 우도 워시드',
+    info: '플로럴, 얼그레이, 살구, 패션후르츠',
+    price: '15,000원',
+    color: '#d06a4f'
+  }, {
+    link: '#',
+    pic: 'item-goods-2.png',
+    title: '바라보다 WEEK 블렌드',
+    info: '초콜렛, 슈가 브라운, 너트',
+    price: '13,000원',
+    color: '#aaa'
+  }, {
+    link: '#',
+    pic: 'item-goods-3.png',
+    title: '바라보다 ANNI 블렌드',
+    info: '베리류, 시트러스, 슈가 브라운',
+    price: '15,000원',
+    color: '#f5601f'
+  }];
 
-    // 공지사항 슬라이드
-    let sw_notice = new Swiper('.sw-notice', {
-        loop: true,
-        slidesPerView: 1,
-        spaceBetween: 30,
-        navigation: {
-            prevEl: '.sw-notice-prev',
-            nextEl: '.sw-notice-next'
-        },
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
-    });
-
-
-}
+  // 아이템 리스트 데이터 화면 출력
+  let itemGoods = $('.item-goods-list > li');
+  let itemTotal = itemGoods.length;
+  for(let i=0; i < itemTotal; i++) {
+    let temp = itemGoods[i];
+    let htmlTxt = `
+      <a href="${itemGoodsArr[i].link}">
+        <img src="images/${itemGoodsArr[i].pic}" art="${itemGoodsArr[i].title}">
+      </a>
+      <div class="item-goods-txt">
+        <a href="${itemGoodsArr[i].link}" class="item-goods-title">${itemGoodsArr[i].title}</a>
+        <span class="item-goods-info" style="color:${itemGoodsArr[i].color}">${itemGoodsArr[i].info}</span>
+        <span class="item-goods-price">${itemGoodsArr[i].price}</span>
+      </div>
+    `;
+    temp.innerHTML = htmlTxt;
+  };
+      
+  // 아이템 리스트 상품설명 부분 색상 교체
+  document.getElementById("c-ecba19").css('color', '#ecba19');
+};
